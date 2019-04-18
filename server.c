55ac8ee9db58bf7b90b2088933f6e3916ddcd130
Andrey <0079036184882@mail.ru>
2019-04-18 22:37:46 +0300

    новые библиотеки

--- server.c
+++ server.c
@@ -1,19 +1,17 @@
 //Server.c
 //Ver 1.0
 
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-//#include <sys/>
+#include <sys/un.h>
+#include <arpa/inet.h>
+#include <netdb.h>  //DNS
 
-
-
-
-
-int main()
+int main(void)
 {
+
+
     return (0);
 }
